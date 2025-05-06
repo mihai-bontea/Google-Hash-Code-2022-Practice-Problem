076fe98d13fc88102a2c484a5cbb1cde21c5f19d
mihai-bontea <mihaibontea@protonmail.com>
2025-05-06 15:48:09 +0300

    smart pointers, move constructor

--- sol1/main.cpp
+++ sol1/main.cpp
@@ -11,48 +11,40 @@
 
 struct SimulationState
 {
-    int depth, clients_lost;
-    std::bitset<MAX_CLIENTS>* is_client_remaining;
-    std::bitset<MAX_INGREDIENTS>* is_ingredient_chosen;
+    int depth = 0, clients_lost = 0;
+    std::unique_ptr<std::bitset<MAX_CLIENTS>> is_client_remaining;
+    std::unique_ptr<std::bitset<MAX_INGREDIENTS>> is_ingredient_chosen;
 
-    SimulationState(): depth(0), clients_lost(0)
+    SimulationState()
+        : is_client_remaining(std::make_unique<std::bitset<MAX_CLIENTS>>())
+        , is_ingredient_chosen(std::make_unique<std::bitset<MAX_INGREDIENTS>>())
     {
-        is_client_remaining = new std::bitset<MAX_CLIENTS>();
-        is_ingredient_chosen = new std::bitset<MAX_INGREDIENTS>();
-
-        (*is_client_remaining).set();
-        (*is_ingredient_chosen).reset();
+        is_client_remaining->set();
+        is_ingredient_chosen->reset();
     }
 
     SimulationState(const SimulationState& other)
-    {
-        depth = other.depth;
-        clients_lost = other.clients_lost;
-
-        is_client_remaining = new std::bitset<MAX_CLIENTS>();
-        is_ingredient_chosen = new std::bitset<MAX_INGREDIENTS>();
-
-        *is_client_remaining = *(other.is_client_remaining);
-        *is_ingredient_chosen = *(other.is_ingredient_chosen);
-    }
+        : depth(other.depth)
+        , clients_lost(other.clients_lost)
+        , is_client_remaining(std::make_unique<std::bitset<MAX_CLIENTS>>(*other.is_client_remaining))
+        , is_ingredient_chosen(std::make_unique<std::bitset<MAX_INGREDIENTS>>(*other.is_ingredient_chosen))
+    {}
 
     SimulationState& operator=(const SimulationState& other)
     {
-        if (this != &other) {
+        if (this != &other)
+        {
             depth = other.depth;
             clients_lost = other.clients_lost;
-
-            *is_client_remaining = *(other.is_client_remaining);
-            *is_ingredient_chosen = *(other.is_ingredient_chosen);
+            *is_client_remaining = *other.is_client_remaining;
+            *is_ingredient_chosen = *other.is_ingredient_chosen;
         }
         return *this;
     }
 
-    ~SimulationState()
-    {
-        delete is_client_remaining;
-        delete is_ingredient_chosen;
-    }
+    SimulationState(SimulationState&&) = default;
+
+    SimulationState& operator=(SimulationState&&) = default;
 };
 
 class Solver
